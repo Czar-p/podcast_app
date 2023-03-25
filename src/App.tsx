ea1d9f31aed7d0fb01afc619e88237ee23a10239
cesarpala <78912306+cesarpala@users.noreply.github.com>
2023-03-25 21:14:04 +0100

    add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import "./styles.css";
-
+import React, { useState } from 'react'
+import './styles.css'
 
 const App = () => {
-  return <h1>Podcaster</h1>;
-};
+  return <h1>Podcaster</h1>
+}
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import * as ReactDOMClient from 'react-dom/client';
-import App from './App';
+import * as ReactDOMClient from 'react-dom/client'
+import App from './App'
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById('root') as HTMLElement
 
-ReactDOMClient.createRoot(container).render(<App />);
+ReactDOMClient.createRoot(container).render(<App />)
